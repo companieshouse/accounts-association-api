eceac1eab845136d395adffcd808a56bf70c1ed6
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2023-12-01 12:18:37 +0000

    IDVA6-357 Initial Infra commit

---
+++ src/main/java/uk/gov/companieshouse/authentication/configuration/InterceptorConfig.java
@@ -0,0 +1,37 @@
+package uk.gov.companieshouse.authentication.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.authentication.interceptor.LoggingInterceptor;
+
+@Configuration
+@ComponentScan("uk.gov.companieshouse.authentication.interceptor")
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    /**
+     * Setup the interceptors to run against endpoints when the endpoints are called
+     * Interceptors are executed in the order they are added to the registry
+     *
+     * @param registry The spring interceptor registry
+     */
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        addLoggingInterceptor(registry);
+    }
+
+    /**
+     * Interceptor that logs all calls to endpoints
+     * @param registry The spring interceptor registry
+     */
+    private void addLoggingInterceptor(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+
+}
---
+++ src/main/java/uk/gov/companieshouse/authentication/models/AuthenticatedEmailAddress.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.authentication.models;
+
+public class AuthenticatedEmailAddress {
+        private String emailAddress;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
