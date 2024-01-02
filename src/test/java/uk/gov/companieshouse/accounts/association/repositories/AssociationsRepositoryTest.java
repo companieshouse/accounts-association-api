b9e7a1cfc2eaf9d23792fc0cec4e50719cfeb38e
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-01-02 17:56:55 +0000

    update to pipeline file

--- src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
@@ -1,25 +1,21 @@
 package uk.gov.companieshouse.accounts.association.repositories;
 
-import org.springframework.dao.DuplicateKeyException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.companieshouse.accounts.association.models.Associations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-@DataMongoTest()
-@ExtendWith(SpringExtension.class)
-@Tag("integration-test")
+@DataMongoTest
 class AssociationsRepositoryTest {
+
 
     @Autowired
     MongoTemplate mongoTemplate;
