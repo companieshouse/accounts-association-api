9c6eb253a4e4f8bf60eea4793bb83fc029f59f35
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-01-04 12:49:25 +0000

    update tests to use test containers

--- src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
@@ -1,22 +1,31 @@
 package uk.gov.companieshouse.accounts.association.repositories;
 
+import jakarta.validation.ConstraintViolationException;
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import uk.gov.companieshouse.accounts.association.models.Associations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-@DataMongoTest
+@SpringBootTest
+@Testcontainers
 class AssociationsRepositoryTest {
 
-
+    @Container
+    @ServiceConnection
+    static MongoDBContainer container = new MongoDBContainer("mongo:4.4.22");
     @Autowired
     MongoTemplate mongoTemplate;
 
@@ -28,23 +37,27 @@ class AssociationsRepositoryTest {
         final var associationOne = new Associations();
         associationOne.setCompanyNumber("111111");
         associationOne.setUserId("111");
+        associationOne.setStatus("New");
 
         final var associationTwo = new Associations();
         associationTwo.setCompanyNumber("222222");
         associationTwo.setUserId("111");
+        associationTwo.setStatus("New");
 
         final var associationThree = new Associations();
         associationThree.setCompanyNumber("111111");
         associationThree.setUserId("222");
+        associationThree.setStatus("New");
 
         associationsRepository.insert(associationOne);
         associationsRepository.insert(associationTwo);
         associationsRepository.insert(associationThree);
+
     }
 
 
     @Test
-    void findAllAssociationsByCompanyNumber() {
+    void findAllAssociationsByCompanyNumberShouldReturnRightTwoAssociations() {
 
         assertThat(associationsRepository.findAllByCompanyNumber("111111")).hasSize(2);
 
@@ -52,23 +65,36 @@ class AssociationsRepositoryTest {
 
 
     @Test
-     void findAllUserId() {
+    void testToNotAllowNotNullStatus() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            final var associationUser = new Associations();
+            associationUser.setCompanyNumber("111111");
+            associationUser.setUserId("111");
+            associationsRepository.save(associationUser);
+        });
+
+    }
+
+    @Test
+    void findAllByUserIdShouldReturnOneAssociation() {
 
         assertThat(associationsRepository.findAllByUserId("222")).hasSize(1);
 
     }
 
-//    @Test
-//    void shouldNotAllowDuplicateAssociations(){
-//
-//        assertThrows(DuplicateKeyException.class, () -> {
-//            final var associationOneUser = new Associations();
-//            associationOneUser.setCompanyNumber("111111");
-//            associationOneUser.setUserId("111");
-//            associationsRepository.insert(associationOneUser);
-//        });
-//
-//    }
+    @Test
+    @Ignore("working in isolation")
+    void shouldNotAllowDuplicateAssociations() {
+
+        assertThrows(DuplicateKeyException.class, () -> {
+            final var associationUser = new Associations();
+            associationUser.setCompanyNumber("111111");
+            associationUser.setUserId("111");
+            associationUser.setStatus("New");
+            associationsRepository.save(associationUser);
+        });
+
+    }
 
     @AfterEach
     public void after() {
