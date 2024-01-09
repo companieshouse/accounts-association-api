321526e8af33566857a99c14fa3ed6360d2b7d4d
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-09 11:46:33 +0000

    Updated the service layer.

---
+++ src/main/java/uk/gov/companieshouse/accounts/association/service/UsersService.java
@@ -0,0 +1,21 @@
+package uk.gov.companieshouse.accounts.association.service;
+
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.accounts.association.models.Users;
+import uk.gov.companieshouse.accounts.association.repositories.UsersRepository;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Optional<Users> fetchUserId( String email ){
+        return usersRepository.fetchUserId( email );
+    }
+
+}
