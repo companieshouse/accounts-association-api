37500739fbbac7df7da468fb89ed9f3113372fe8
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-09 11:37:47 +0000

    Updated the data layer.

--- src/main/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepository.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepository.java
@@ -1,8 +1,10 @@
 package uk.gov.companieshouse.accounts.association.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.companieshouse.accounts.association.models.Associations;
+import org.springframework.data.mongodb.core.query.Update;
 
 @Repository
 public interface AssociationsRepository extends MongoRepository<Associations, String> {
@@ -10,5 +12,11 @@ public interface AssociationsRepository extends MongoRepository<Associations, St
     Iterable<Associations> findAllByCompanyNumber( final String companyNumber );
 
     Iterable<Associations> findAllByUserId( final String userId );
+
+    @Query( value = "{ 'userId': ?0, 'companyNumber': ?1 }", exists = true )
+    boolean associationExists( String userId, String companyNumber );
+
+    @Query( "{ 'userId': ?0, 'companyNumber': ?1 }" )
+    void updateAssociation( final String userId, final String companyNumber, Update update );
 
 }
