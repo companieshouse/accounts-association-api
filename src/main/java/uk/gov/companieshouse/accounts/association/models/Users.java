dc079657cf4385ea3a74664f425e86582ede1650
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-09 11:18:01 +0000

    Updated pom to import up-to-date interfaces. Added Kartheek's StatusEnum class, and created a new contains method in it. Added Suvarna's Users class.

---
+++ src/main/java/uk/gov/companieshouse/accounts/association/models/Users.java
@@ -0,0 +1,66 @@
+package uk.gov.companieshouse.accounts.association.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "users")
+public class Users {
+    @Id
+    private String id;
+
+    @Field("surname")
+    private String surname;
+
+    @Field("forename")
+    private String forename;
+
+    @Field("email")
+    private String email;
+
+    @Field("display_name")
+    private String displayName;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
