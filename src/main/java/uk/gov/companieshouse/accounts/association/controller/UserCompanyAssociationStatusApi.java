b088882c78099d57015a6e8fb4362b8dd89d4103
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-10 11:55:27 +0000

    Ported over ControllerAdvice from Identity Verification project. The ControllerAdvice code from the Identity Verification project did handle the ConstraintViolationException case, so I have added my own.
    Created the Rest Handler layer.
    
    There is an error in the specification which has led to incorrectly generated code in the interface. This makes it impossible to run the tests correctly. Will need to update the spec and then try and run the tests.

---
+++ src/main/java/uk/gov/companieshouse/accounts/association/controller/UserCompanyAssociationStatusApi.java
@@ -0,0 +1,66 @@
+package uk.gov.companieshouse.accounts.association.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.companieshouse.accounts.association.AccountsAssociationServiceApplication;
+import uk.gov.companieshouse.accounts.association.enums.StatusEnum;
+import uk.gov.companieshouse.accounts.association.exceptions.BadRequestRuntimeException;
+import uk.gov.companieshouse.accounts.association.exceptions.NotFoundRuntimeException;
+import uk.gov.companieshouse.accounts.association.service.AssociationsService;
+import uk.gov.companieshouse.accounts.association.service.UsersService;
+import uk.gov.companieshouse.api.accounts.associations.api.UserCompanyAssociationStatusInterface;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+@RestController
+public class UserCompanyAssociationStatusApi implements UserCompanyAssociationStatusInterface {
+
+    private final AssociationsService associationsService;
+    private final UsersService usersService;
+
+    private static final Logger LOG = LoggerFactory.getLogger(AccountsAssociationServiceApplication.APPLICATION_NAME_SPACE);
+
+    public UserCompanyAssociationStatusApi(AssociationsService associationsService,
+            UsersService usersService) {
+        this.associationsService = associationsService;
+        this.usersService = usersService;
+    }
+
+    @Override
+    public ResponseEntity<Void> updateAssociationStatusForUserAndCompany(
+           String userEmail, String companyNumber, String status, String xRequestId) {
+
+        LOG.debug( String.format( "%s: Attempting to update association status between user (%s) and company (%s)...", xRequestId, userEmail, companyNumber ) );
+
+        if ( !StatusEnum.contains( status ) ){
+            LOG.error( String.format( "%s: Invalid status provided (%s)", xRequestId, status ) );
+            throw new BadRequestRuntimeException( "Status must be either 'Confirmed' or 'Deleted'" );
+        }
+
+        final var userOptional = usersService.fetchUserId( userEmail );
+        if ( userOptional.isEmpty() ) {
+            LOG.error( String.format( "%s: Unable to find user with the email address: %s.", xRequestId, userEmail ) );
+            throw new NotFoundRuntimeException( "user_email", String.format( "Could not find user with email address: %s.", userEmail)  );
+        }
+        final var userId = userOptional.get().getId();
+
+        if ( !associationsService.associationExists( userId, companyNumber ) ) {
+            LOG.error( String.format( "%s: Unable to find association where companyNumber is %s, and userId is %s", xRequestId, companyNumber, userId ) );
+            throw new NotFoundRuntimeException( "association", String.format( "Could not find association where companyNumber is %s, and userId is %s.", companyNumber, userId )  );
+        }
+
+        if ( status.equals( StatusEnum.CONFIRMED.getValue() ) )
+            associationsService.confirmAssociation( userId, companyNumber );
+        else if ( status.equals( StatusEnum.DELETED.getValue() ) )
+            associationsService.softDeleteAssociation( userId, companyNumber );
+        else
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        LOG.debug( String.format( "%s: Updated association status between user (%s) and company (%s)...", xRequestId, userEmail, companyNumber ) );
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // TODO: this class( email regex ), run tests for this class
+}
