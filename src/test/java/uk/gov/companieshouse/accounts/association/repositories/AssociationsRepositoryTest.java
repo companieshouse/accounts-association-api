e4ac76360fb92e73a286c64c545757870ae8c764
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-01-11 11:23:54 +0000

    changes to controller and add support to private sdk

--- src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/repositories/AssociationsRepositoryTest.java
@@ -1,31 +1,28 @@
 package uk.gov.companieshouse.accounts.association.repositories;
 
 import jakarta.validation.ConstraintViolationException;
-import org.junit.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.companieshouse.accounts.association.models.Association;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-@SpringBootTest
-@Testcontainers
+@DataMongoTest()
+@ExtendWith(SpringExtension.class)
+@Tag("integration-test")
 class AssociationsRepositoryTest {
 
-    @Container
-    @ServiceConnection
-    static MongoDBContainer container = new MongoDBContainer("mongo:4.4.22").withExposedPorts(27017);
     @Autowired
     MongoTemplate mongoTemplate;
 
@@ -60,18 +57,6 @@ class AssociationsRepositoryTest {
     void findAllAssociationsByCompanyNumberShouldReturnRightTwoAssociations() {
 
         assertThat(associationsRepository.findAllByCompanyNumber("111111")).hasSize(2);
-
-    }
-
-
-    @Test
-    void testToNotAllowNotNullStatus() {
-        assertThrows(ConstraintViolationException.class, () -> {
-            final var associationUser = new Association();
-            associationUser.setCompanyNumber("111111");
-            associationUser.setUserId("111");
-            associationsRepository.save(associationUser);
-        });
 
     }
 
