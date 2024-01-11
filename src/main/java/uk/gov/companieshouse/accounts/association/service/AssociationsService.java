cb9f7945f17f7350f055e6ac0c78857cc9a40cf9
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-11 15:21:54 +0000

    Updated the Service Layer to account for new logic.

--- src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
@@ -18,16 +18,20 @@ public class AssociationsService {
         return associationsRepository.associationExists( userId, companyNumber );
     }
 
-    public void softDeleteAssociation( final String userId, final String companyNumber ){
-        final var update = new Update()
-                .set( "status", "Deleted" )
+    public void softDeleteAssociation( final String userId, final String companyNumber, final boolean userInfoExists ){
+        var update = new Update()
+                .set( "status", "Removed" )
                 .set( "deletionTime", LocalDateTime.now() );
+
+        if ( userInfoExists )
+            update = update.set( "temporary", false );
 
         associationsRepository.updateAssociation( userId, companyNumber, update );
     }
 
     public void confirmAssociation( final String userId, final String companyNumber ){
         final var update = new Update()
+                .set( "temporary", false )
                 .set( "status", "Confirmed" )
                 .set( "confirmationApprovalTime", LocalDateTime.now() );
 
