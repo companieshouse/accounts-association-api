96f745124033f0570e7bb744b3bef906a564f0ae
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-11 15:21:54 +0000

    Updated the Service Layer to account for new logic.

--- src/main/java/uk/gov/companieshouse/accounts/association/service/UsersService.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/service/UsersService.java
@@ -2,20 +2,28 @@ package uk.gov.companieshouse.accounts.association.service;
 
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.accounts.association.models.UserInfo;
 import uk.gov.companieshouse.accounts.association.models.Users;
 import uk.gov.companieshouse.accounts.association.repositories.UsersRepository;
+import uk.gov.companieshouse.accounts.association.rest.UserInfoMockEndpoint;
 
 @Service
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final UserInfoMockEndpoint userInfoMockEndpoint;
 
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, UserInfoMockEndpoint userInfoMockEndpoint) {
         this.usersRepository = usersRepository;
+        this.userInfoMockEndpoint = userInfoMockEndpoint;
     }
 
     public Optional<Users> fetchUserId( String email ){
         return usersRepository.fetchUserId( email );
+    }
+
+    public Optional<UserInfo> fetchUserInfo( final String userEmail ){
+        return userInfoMockEndpoint.fetchUserInfo( userEmail );
     }
 
 }
