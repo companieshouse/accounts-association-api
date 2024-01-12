fcccffb4eb5b90844fc73cd85901dcdcf755b929
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-12 14:01:46 +0000

    Merged Code

--- src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
@@ -23,16 +23,20 @@ public class AssociationsService {
         return associationsRepository.findByUserIdAndCompanyNumber(userId, companyNumber);
     }
 
-    public void softDeleteAssociation(final String userId, final String companyNumber) {
-        final var update = new Update()
-                .set("status", StatusEnum.REMOVED.getValue())
-                .set("deletionTime", LocalDateTime.now());
+    public void softDeleteAssociation( final String userId, final String companyNumber, final boolean userInfoExists ){
+        var update = new Update()
+                .set( "status", StatusEnum.REMOVED.getValue() )
+                .set( "deletionTime", LocalDateTime.now() );
+
+        if ( userInfoExists )
+            update = update.set( "temporary", false );
 
         associationsRepository.updateAssociation(userId, companyNumber, update);
     }
 
     public void confirmAssociation(final String userId, final String companyNumber) {
         final var update = new Update()
+                .set( "temporary", false )
                 .set("status", StatusEnum.CONFIRMED.getValue())
                 .set("confirmationApprovalTime", LocalDateTime.now());
 
