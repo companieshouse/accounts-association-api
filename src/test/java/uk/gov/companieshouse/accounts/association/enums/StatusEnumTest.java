2f329302f5140173f73a232196eae2049ad18b27
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-01-15 15:09:38 +0000

    code review changes

--- src/test/java/uk/gov/companieshouse/accounts/association/enums/StatusEnumTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/enums/StatusEnumTest.java
@@ -2,20 +2,19 @@ package uk.gov.companieshouse.accounts.association.enums;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import uk.gov.companieshouse.accounts.association.enums.StatusEnum;
 
-public class StatusEnumTest {
+class StatusEnumTest {
 
     @Test
-    void containsWithInvalidAssociationStatusReturnsFalse(){
-        Assertions.assertFalse( StatusEnum.contains( null ) );
-        Assertions.assertFalse( StatusEnum.contains( "It's complicated..." ) );
+    void containsWithInvalidAssociationStatusReturnsFalse() {
+        Assertions.assertFalse(StatusEnum.contains(null));
+        Assertions.assertFalse(StatusEnum.contains("It's complicated..."));
     }
 
     @Test
-    void containsWithValidAssociationStatusReturnsTrue(){
-        Assertions.assertTrue( StatusEnum.contains( "Confirmed" ) );
-        Assertions.assertTrue( StatusEnum.contains( "Removed" ) );
+    void containsWithValidAssociationStatusReturnsTrue() {
+        Assertions.assertTrue(StatusEnum.contains("Confirmed"));
+        Assertions.assertTrue(StatusEnum.contains("Removed"));
     }
 
 }
