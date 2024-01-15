72695794e853bf22cb8b9577ab5241dc2b4668a8
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-01-15 11:53:09 +0000

    Updated Association @Document annotation. Added internal server error case to ControllerAdvice. Added several unit tests.

--- src/test/java/uk/gov/companieshouse/accounts/association/controllers/UserCompanyAssociationStatusApiTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/controllers/UserCompanyAssociationStatusApiTest.java
@@ -37,7 +37,7 @@ public class UserCompanyAssociationStatusApiTest {
     @Autowired
     public MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     AssociationsRepository associationsRepository;
 
     @BeforeEach
