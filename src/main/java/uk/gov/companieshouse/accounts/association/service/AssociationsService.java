8b081f79d3b5533018aca3cf5f428ab2d9fe8044
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-03-19 15:16:16 +0000

    IDVA6-494: Ported over service layer and rest layer again

--- src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/service/AssociationsService.java
@@ -1,9 +1,12 @@
 package uk.gov.companieshouse.accounts.association.service;
 
 import jakarta.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.companieshouse.accounts.association.mapper.AssociationMapper;
@@ -12,6 +15,7 @@ import uk.gov.companieshouse.accounts.association.mapper.AssociationsListUserMap
 import uk.gov.companieshouse.accounts.association.models.AssociationDao;
 import uk.gov.companieshouse.accounts.association.repositories.AssociationsRepository;
 import uk.gov.companieshouse.api.accounts.associations.model.Association;
+import uk.gov.companieshouse.api.accounts.associations.model.Association.StatusEnum;
 import uk.gov.companieshouse.api.accounts.associations.model.AssociationsList;
 import uk.gov.companieshouse.api.accounts.user.model.User;
 
@@ -19,6 +23,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import uk.gov.companieshouse.api.company.CompanyDetails;
 
 @Service
 public class AssociationsService {
@@ -62,5 +67,16 @@ public class AssociationsService {
         return associationsListUserMapper.daoToDto(results, user);
     }
 
+    public AssociationsList fetchAssociatedUsers( final String companyNumber, final CompanyDetails companyDetails, final boolean includeRemoved, final int itemsPerPage, final int pageIndex ){
+        final Pageable pageable = PageRequest.of( pageIndex, itemsPerPage );
+
+        final var statuses = new HashSet<>( Set.of( StatusEnum.CONFIRMED.getValue(), StatusEnum.AWAITING_APPROVAL.getValue() ) );
+        if ( includeRemoved )
+            statuses.add( StatusEnum.REMOVED.getValue() );
+
+        final var associations = associationsRepository.fetchAssociatedUsers( companyNumber, statuses, pageable );
+
+        return associationsListCompanyMapper.daoToDto( associations, companyDetails );
+    }
 
 }
