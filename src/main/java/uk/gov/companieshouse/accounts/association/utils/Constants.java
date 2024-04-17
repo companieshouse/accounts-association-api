2abe7f4c9658121aa024f77889d969e72563c37a
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-04-17 09:41:45 +0100

    Added models and constants

---
+++ src/main/java/uk/gov/companieshouse/accounts/association/utils/Constants.java
@@ -0,0 +1,17 @@
+package uk.gov.companieshouse.accounts.association.utils;
+
+public class Constants {
+
+    public static final String AUTH_CODE_CONFIRMATION_MESSAGE_TYPE = "associations_authorised_person_added_themselves";
+
+    public static final String AUTHORISATION_REMOVED_MESSAGE_TYPE = "associations_authorised_person_removed";
+
+    public static final String INVITATION_CANCELLED_MESSAGE_TYPE = "associations_invitation_cancelled";
+
+    public static final String INVITATION_MESSAGE_TYPE = "associations_invited_to_be_authorised";
+
+    public static final String INVITATION_ACCEPTED_MESSAGE_TYPE = "associations_invitee_accepted";
+
+    public static final String INVITATION_REJECTED_MESSAGE_TYPE = "associations_invitee_declined";
+
+}
