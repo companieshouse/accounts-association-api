eea03ea988bb40076b27f847aaa72550f0d37011
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-05-02 11:04:56 +0100

    Added tests and did a minor refactoring to email service

--- src/test/java/uk/gov/companieshouse/accounts/association/models/email/senders/AuthCodeConfirmationEmailSenderTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/models/email/senders/AuthCodeConfirmationEmailSenderTest.java
@@ -3,6 +3,8 @@ package uk.gov.companieshouse.accounts.association.models.email.senders;
 import static org.mockito.ArgumentMatchers.any;
 import static uk.gov.companieshouse.accounts.association.utils.Constants.AUTH_CODE_CONFIRMATION_MESSAGE_TYPE;
 
+import java.util.List;
+import java.util.function.Supplier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.accounts.association.models.email.data.AuthCodeConfirmationEmailData;
+import uk.gov.companieshouse.api.accounts.user.model.User;
+import uk.gov.companieshouse.api.company.CompanyDetails;
 import uk.gov.companieshouse.email_producer.EmailProducer;
 import uk.gov.companieshouse.email_producer.EmailSendingException;
 
@@ -52,6 +56,67 @@ public class AuthCodeConfirmationEmailSenderTest {
 
         authCodeConfirmationEmailSender.sendEmail( "Krishna Patel", "Tesla" ).accept( "kpatel@companieshouse.gov.uk" );
         Mockito.verify( emailProducer ).sendEmail( emailData, AUTH_CODE_CONFIRMATION_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersWithNullCompanyDetailsOrNullCompanyNameOrNullDisplayNameOrNullRequestsThrowsNullPointerException(){
+        final var emailData = new AuthCodeConfirmationEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Krishna Patel is now authorised to file online for Tesla" );
+        emailData.setAuthorisedPerson( "Krishna Patel" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+
+        Assertions.assertThrows( NullPointerException.class, () -> authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", null, "Krishna Patel", requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", new CompanyDetails(), "Krishna Patel", requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, null, requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Krishna Patel", null ) );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersThrowsEmailOnKafkaQueue(){
+        final var emailData = new AuthCodeConfirmationEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Krishna Patel is now authorised to file online for Tesla" );
+        emailData.setAuthorisedPerson( "Krishna Patel" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+        authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Krishna Patel", requestsToFetchAssociatedUsers );
+
+        Mockito.verify( emailProducer ).sendEmail( emailData, AUTH_CODE_CONFIRMATION_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersDoesNotThrowEmailOnKafkaQueue(){
+        final var emailData = new AuthCodeConfirmationEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Krishna Patel is now authorised to file online for Tesla" );
+        emailData.setAuthorisedPerson( "Krishna Patel" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Krishna Patel", List.of() );
+
+        Mockito.verify( emailProducer, Mockito.never() ).sendEmail( emailData, AUTH_CODE_CONFIRMATION_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersWithUnexpectedIssueThrowsEmailSendingException(){
+        final var emailData = new AuthCodeConfirmationEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Krishna Patel is now authorised to file online for Tesla" );
+        emailData.setAuthorisedPerson( "Krishna Patel" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+
+        Mockito.doThrow( new EmailSendingException( "Failed to send email", new Exception() ) ).when( emailProducer ).sendEmail( any(), any() );
+        Assertions.assertThrows( EmailSendingException.class, () -> authCodeConfirmationEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Krishna Patel", requestsToFetchAssociatedUsers ) );
     }
 
 }
--- src/test/java/uk/gov/companieshouse/accounts/association/models/email/senders/InvitationRejectedEmailSenderTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/association/models/email/senders/InvitationRejectedEmailSenderTest.java
@@ -3,6 +3,8 @@ package uk.gov.companieshouse.accounts.association.models.email.senders;
 import static org.mockito.ArgumentMatchers.any;
 import static uk.gov.companieshouse.accounts.association.utils.Constants.INVITATION_REJECTED_MESSAGE_TYPE;
 
+import java.util.List;
+import java.util.function.Supplier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.accounts.association.models.email.data.InvitationRejectedEmailData;
+import uk.gov.companieshouse.api.accounts.user.model.User;
+import uk.gov.companieshouse.api.company.CompanyDetails;
 import uk.gov.companieshouse.email_producer.EmailProducer;
 import uk.gov.companieshouse.email_producer.EmailSendingException;
 
@@ -52,6 +56,67 @@ public class InvitationRejectedEmailSenderTest {
 
         invitationRejectedEmailSender.sendEmail( "Elon Musk", "Tesla" ).accept( "kpatel@companieshouse.gov.uk" );
         Mockito.verify( emailProducer ).sendEmail( emailData, INVITATION_REJECTED_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersWithNullCompanyDetailsOrNullCompanyNameOrNullDisplayNameOrNullRequestsThrowsNullPointerException(){
+        final var emailData = new InvitationRejectedEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Elon Musk has declined to be digitally authorised to file online for Tesla" );
+        emailData.setPersonWhoDeclined( "Elon Musk" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+
+        Assertions.assertThrows( NullPointerException.class, () -> invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", null, "Elon Musk", requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", new CompanyDetails(), "Elon Musk", requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, null, requestsToFetchAssociatedUsers ) );
+        Assertions.assertThrows( NullPointerException.class, () -> invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Elon Musk", null ) );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersThrowsEmailOnKafkaQueue(){
+        final var emailData = new InvitationRejectedEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Elon Musk has declined to be digitally authorised to file online for Tesla" );
+        emailData.setPersonWhoDeclined( "Elon Musk" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+        invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Elon Musk", requestsToFetchAssociatedUsers );
+
+        Mockito.verify( emailProducer ).sendEmail( emailData, INVITATION_REJECTED_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersDoesNotThrowEmailOnKafkaQueue(){
+        final var emailData = new InvitationRejectedEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Elon Musk has declined to be digitally authorised to file online for Tesla" );
+        emailData.setPersonWhoDeclined( "Elon Musk" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Elon Musk", List.of() );
+
+        Mockito.verify( emailProducer, Mockito.never() ).sendEmail( emailData, INVITATION_REJECTED_MESSAGE_TYPE );
+    }
+
+    @Test
+    void sendEmailToAssociatedUsersWithUnexpectedIssueThrowsEmailSendingException(){
+        final var emailData = new InvitationRejectedEmailData();
+        emailData.setTo( "kpatel@companieshouse.gov.uk" );
+        emailData.setSubject( "Companies House: Elon Musk has declined to be digitally authorised to file online for Tesla" );
+        emailData.setPersonWhoDeclined( "Elon Musk" );
+        emailData.setCompanyName( "Tesla" );
+
+        final var companyDetails = new CompanyDetails().companyName( "Tesla" );
+        List<Supplier<User>> requestsToFetchAssociatedUsers = List.of( () -> new User().email( "kpatel@companieshouse.gov.uk" ) );
+
+        Mockito.doThrow( new EmailSendingException( "Failed to send email", new Exception() ) ).when( emailProducer ).sendEmail( any(), any() );
+        Assertions.assertThrows( EmailSendingException.class, () -> invitationRejectedEmailSender.sendEmailToAssociatedUsers( "theId12345", companyDetails, "Elon Musk", requestsToFetchAssociatedUsers ) );
     }
 
 }
