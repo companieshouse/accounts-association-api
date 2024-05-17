d4c0e9f8040d444b7c93af041bc8e283a9fd3cdf
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-05-17 15:06:26 +0100

    Fixed the AuthorizationInterceptor and all of the failing tests. Also refactored tests.

--- src/main/java/uk/gov/companieshouse/accounts/association/interceptor/AuthorizationInterceptor.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/interceptor/AuthorizationInterceptor.java
@@ -2,60 +2,59 @@ package uk.gov.companieshouse.accounts.association.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Objects;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import uk.gov.companieshouse.accounts.association.exceptions.NotFoundRuntimeException;
 import uk.gov.companieshouse.accounts.association.models.UserContext;
 import uk.gov.companieshouse.accounts.association.service.UsersService;
 import uk.gov.companieshouse.accounts.association.utils.StaticPropertyUtil;
-import uk.gov.companieshouse.api.accounts.user.model.User;
 import uk.gov.companieshouse.api.util.security.AuthorisationUtil;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
-import java.util.Objects;
-
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
 
+    private final UsersService usersService;
 
-    @Autowired
-    UsersService usersService;
-
+    public AuthorizationInterceptor(UsersService usersService) {
+        this.usersService = usersService;
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StaticPropertyUtil.APPLICATION_NAMESPACE);
 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-
         return isOauth2User(request, response) && isValidAuthorisedUser(request, response);
-
     }
 
     private boolean isOauth2User(HttpServletRequest request, HttpServletResponse response) {
-
-        if (!AuthorisationUtil.isOauth2User(request)) {
-            LOGGER.debugRequest(request, "invalid user", null);
-            response.setStatus(401);
-            return false;
+        final var hasEricIdentity = !Objects.isNull( request.getHeader( "Eric-Identity" ) );
+        final var hasEricIdentityType = !Objects.isNull( request.getHeader( "Eric-Identity-Type" ) );
+        if ( hasEricIdentity && hasEricIdentityType && AuthorisationUtil.isOauth2User(request) ) {
+            return true;
         }
 
-        return true;
-
+        LOGGER.debugRequest(request, "invalid user", null);
+        response.setStatus(401);
+        return false;
     }
 
     private boolean isValidAuthorisedUser(HttpServletRequest request, HttpServletResponse response) {
         String identity = AuthorisationUtil.getAuthorisedIdentity(request);
-        final User user = usersService.fetchUserDetails(identity);
-        if (Objects.nonNull(user)) {
-            UserContext.setLoggedUser(user);
+
+        try {
+            final var userDetails = usersService.fetchUserDetails( identity );
+            UserContext.setLoggedUser( userDetails );
             return true;
+        } catch ( NotFoundRuntimeException e ){
+            LOGGER.debugRequest(request, "no user found with identity [" + identity + "]", null);
+            response.setStatus(403);
+            return false;
         }
-        LOGGER.debugRequest(request, "no user found with identity [" + identity + "]", null);
-        response.setStatus(403);
-        return false;
 
     }
 
