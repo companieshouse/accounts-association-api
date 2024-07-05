f8ab32aa2f852e8514f47a32b69bcbb81b037f91
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-07-05 13:43:35 +0100

    roll back loggers

--- src/main/java/uk/gov/companieshouse/accounts/association/rest/AccountsUserEndpoint.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/rest/AccountsUserEndpoint.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.accounts.association.utils.ApiClientUtil;
-import uk.gov.companieshouse.accounts.association.utils.StaticPropertyUtil;
 import uk.gov.companieshouse.api.accounts.user.model.User;
 import uk.gov.companieshouse.api.accounts.user.model.UsersList;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.accountsuser.request.PrivateAccountsUserUserGet;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.ApiResponse;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 
 import java.util.List;
 
@@ -23,7 +20,6 @@ public class AccountsUserEndpoint {
     private String accountApiUrl;
 
     private final ApiClientUtil apiClientUtil;
-    protected static final Logger LOG = LoggerFactory.getLogger(StaticPropertyUtil.APPLICATION_NAMESPACE);
 
     @Autowired
     public AccountsUserEndpoint(ApiClientUtil apiClientUtil) {
@@ -32,7 +28,6 @@ public class AccountsUserEndpoint {
 
     public ApiResponse<UsersList> searchUserDetails(final List<String> emails) throws ApiErrorResponseException, URIValidationException {
 
-        LOG.infoContext("internal api url : "+ accountApiUrl, "", null);
         final var searchUserDetailsUrl = "/users/search";
         return apiClientUtil.getInternalApiClient(accountApiUrl)
                 .privateAccountsUserResourceHandler()
@@ -41,7 +36,6 @@ public class AccountsUserEndpoint {
     }
 
     public PrivateAccountsUserUserGet createGetUserDetailsRequest(final String userId) {
-        LOG.infoContext("internal api url : "+ accountApiUrl, "", null);
         final var getUserDetailsUrl = String.format("/users/%s", userId);
         return apiClientUtil.getInternalApiClient(accountApiUrl)
                             .privateAccountsUserResourceHandler()
