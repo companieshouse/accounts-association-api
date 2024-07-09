b30a4f093fa9dc78fe17a723317d3485295a8c2c
kkonuganti-ch <kkonuganti@companieshouse.gov.uk>
2024-07-09 11:54:08 +0100

    amend api client config to take multiple endpoints with new api key

--- src/main/java/uk/gov/companieshouse/accounts/association/utils/ApiClientUtil.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/utils/ApiClientUtil.java
@@ -1,20 +1,18 @@
 package uk.gov.companieshouse.accounts.association.utils;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
 
 
 @Component
 public class ApiClientUtil {
 
-    @Value("${chs.internal.api.key}")
-    private String internalApiKey;
+    @Autowired
+    InternalApiClient internalApiClient;
 
 
     public InternalApiClient getInternalApiClient(final String internalApiUrl) {
-        final var internalApiClient = new InternalApiClient(new ApiKeyHttpClient(internalApiKey));
         internalApiClient.setInternalBasePath(internalApiUrl);
         return internalApiClient;
     }
