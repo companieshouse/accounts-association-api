fef2422d40c6878ce01cf6950e11c0cf1a47a394
kpatel-ch <kpatel@companieshouse.gov.uk>
2025-07-01 09:35:55 +0100

    SIV-395: Added rest of the skeleton

--- src/main/java/uk/gov/companieshouse/accounts/association/controller/UserCompanyAssociation.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/controller/UserCompanyAssociation.java
@@ -114,6 +114,7 @@ public class UserCompanyAssociation implements UserCompanyAssociationInterface {
                         .map( status -> mapToConfirmedUpdate( targetAssociation, targetUser, getEricIdentity() ) )
                         .orElseThrow( () -> new BadRequestRuntimeException( "requesting user does not have access to perform the action", new Exception( "Requesting user cannot change their status from migrated to confirmed" ) ) );
                 case REMOVED -> mapToRemovedUpdate( targetAssociation, targetUser, getEricIdentity() );
+                case UNAUTHORISED -> null; // TODO
             };
         } else {
             update = switch ( requestBody.getStatus() ){
@@ -126,6 +127,7 @@ public class UserCompanyAssociation implements UserCompanyAssociationInterface {
                         .filter( status -> associationsService.confirmedAssociationExists( targetAssociation.getCompanyNumber(), getEricIdentity() ) || hasAdminPrivilege( ADMIN_UPDATE_PERMISSION ) )
                         .map( status -> mapToRemovedUpdate( targetAssociation, targetUser, getEricIdentity() ) )
                         .orElseThrow( () -> new BadRequestRuntimeException( "requesting user does not have access to perform the action", new Exception( String.format( "Requesting %s user cannot change another user to confirmed or the requesting user is not associated with company %s", getEricIdentity(), targetAssociation.getCompanyNumber() ) ) ) );
+                case UNAUTHORISED -> null; // TODO
             };
         }
         associationsService.updateAssociation( targetAssociation.getId(), update );
