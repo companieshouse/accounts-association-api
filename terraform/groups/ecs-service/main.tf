1d148dc03f1cd1c1ecb9f49fae07699a96765618
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2025-07-03 14:49:02 +0100

    CHANGING LOAD BALANCERS TO MULTI-LB

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.296"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.326"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
@@ -28,7 +28,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.296"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.326"
 
   # Environmental configuration
   environment             = var.environment
@@ -42,9 +42,18 @@ module "ecs-service" {
   lb_listener_arn                   = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority         = local.lb_listener_rule_priority
   lb_listener_paths                 = local.lb_listener_paths
-  healthcheck_healthy_threshold     = "2"
-  health_check_grace_period_seconds = 240
-
+  multilb_setup             = true
+  multilb_listeners         = {
+    "priva-api-lb": {
+      listener_arn            = data.aws_lb_listener.secondary_lb_listener.arn
+      load_balancer_arn       = data.aws_lb.secondary_lb.arn
+    }
+    "pub-api-lb": {
+      listener_arn           = data.aws_lb_listener.service_lb_listener.arn
+      load_balancer_arn      = data.aws_lb.service_lb.arn
+    }
+  }
+  
   # Service Healthcheck configuration
   use_task_container_healthcheck    = true
   healthcheck_path                  = local.healthcheck_path
@@ -73,7 +82,7 @@ module "ecs-service" {
   use_capacity_provider              = var.use_capacity_provider
 
   # Cloudwatch
-  cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
+  multilb_cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
   task_environment          = local.task_environment
